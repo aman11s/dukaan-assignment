bd82842ee8f457abfc06aa0624ca306a60bc438b
Aman Singh <reachout.amansingh@gmail.com>
2022-11-22 03:19:08 +0530

    docs : add other products data

--- src/db/index.js
+++ src/db/index.js
@@ -1,2 +1,3 @@
 export { slogansData } from "./slogans-data";
 export { featuresData } from "./features-data";
+export { productsData } from "./products-data";
---
+++ src/db/products-data.js
@@ -0,0 +1,38 @@
+import privacyPolicy from "../assets/products/privacy-policy.svg";
+import termsAndConditions from "../assets/products/terms-and-conditions.svg";
+import domainName from "../assets/products/domain-name.svg";
+import invoice from "../assets/products/invoice.svg";
+
+export const productsData = [
+  {
+    id: 1,
+    img: privacyPolicy,
+    label: "Privacy Policy Generator",
+    description:
+      "Stock your store with 100s of products and start selling to customers in minutes, without the hassle of inventory or packaging.",
+  },
+
+  {
+    id: 2,
+    img: termsAndConditions,
+    label: "Terms & Conditions Generator",
+    description:
+      "Stock your store with 100s of products and start selling to customers in minutes, without the hassle of inventory or packaging.",
+  },
+
+  {
+    id: 3,
+    img: domainName,
+    label: "Domain Name Generator",
+    description:
+      "Stock your store with 100s of products and start selling to customers in minutes, without the hassle of inventory or packaging.",
+  },
+
+  {
+    id: 4,
+    img: invoice,
+    label: "Invoice Generator",
+    description:
+      "Stock your store with 100s of products and start selling to customers in minutes, without the hassle of inventory or packaging.",
+  },
+];
